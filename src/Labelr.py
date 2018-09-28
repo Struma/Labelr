93a1702433671da59e5aba50b74dc34bf473edf8
Struma <Struma_dev@protonmail.com>
2018-09-28 00:53:54 -0400

    IPTC pasring becomes a thing

--- src/Labelr.py
+++ src/Labelr.py
@@ -12,6 +12,8 @@ from optparse import OptionParser
 import os
 from PIL import Image, ImageDraw, ImageFont
 import re
+import pyexiv2
+import datetime
 
 
 
@@ -23,16 +25,32 @@ def main():
     # Option Parser 
     #
     usage = "usage: %prog [options] <input> <output_dir>"
+
     parser = OptionParser(usage=usage)
+
+
     parser.add_option("-f", "--file", action="store",type="string", dest="filename",
             help="write report to FILE", metavar="FILENAME")
+
+
     parser.add_option("-q", "--quiet",
             action="store_false", dest="verbose",
             default=True,
             help="don't print status messages to stdout")
+
+
+    parser.add_option("-A", "--author",        # AUTHOR input
+            action="store_true", dest="auth",
+            default=False,
+            help="Prompt the user for author name")
+
+
+
     parser.add_option("-d", "--details", action="store",type="string",
             dest="details_file",
             help="Details for labeling", metavar="detail_file")
+
+
     (options, args) = parser.parse_args()
 
 
@@ -55,46 +73,99 @@ def main():
         raise RuntimeError("No source image or source directory specified:\
 Labelr.py --help for help")
 
+    # Start going through the options
+    #
+    if options.auth:
+        AUTHOR = prompt_auth() #Author will be treated as a constant once set.
 
     # save resized images into new folder
     #
     for img in imgs:
-        size_img(img).save(args[1] + '/' + os.path.basename(img).split('.')[0] + ".jpg")
+        image_file = size_img(img)
 
-#--------------------------------------------------------------------------
-# Definitons for label creation
-#
-def default():
-    return "default case"
+        if options.auth:
+            # draw subtitle
+            image_file = draw_title(image_file,
+                                    os.path.basename(img).split('.')[0],
+                                    AUTHOR)
+        else:
+            image_file = draw_title(image_file, os.path.basename(img).split('.')[0])
+        image_file.show()
 
-def interactive():
-    return ("interactive case")
 
-def sup_name():
-    pass
+def prompt_auth():
+    author = input("Name of Systematician\n--> ")
+    return author
 
-# Dictionary map to call appropriate label creation
-#
-def label_maker(case = 0):
+def IPTC_Writer(image_flname,
+                descrip = "NA",
+                digitizer = "NA",
+                origin = "NA",
+                gen = "NA",
+                email = "NA",
+                barcode = "NA",
+                spec = "NA",
+                herb_url = "NA",
+                AOFP_url = "NA"):
 
-    switcher = {
-            0: default,
-            1: interactive,
-            2: sup_name
-            }
+    j = pyexiv2.ImageMetadata(image_flname)
+    j.read()
+    j['Iptc.Application2.Subject'] = ["Description: " + descrip, "Origin: " + origin, "Genus: " + gen,
+     "Species: " + spec, "Barcode: " + barcode , herb_url, "AOFP: " + AOFP_url]
+    j['Iptc.Application2.DateCreated'] = [datetime.date.today()]
+    j['Iptc.Application2.Byline'] = [digitizer]
+    j['Iptc.Application2.Headline'] = ["FSU Herbarium Specimen: Plant Seeds- Genus: " + gen + " Spec: " + spec]
+    j['Iptc.Application2.Contact'] = [email]
+    j.write()
+'''
+program_description = "Project; SEEDY project by Daniel Dominguez, Open Source python tools for quick digitizing and authoring of HQ Herbarium seed images."
+digitiz ="Digitizer: Daniel Dominguez"
 
-    # Get the function from switcher dictionary
-    func = switcher.get(case, "nothing")
-    # Execute the function
-    print(func())
-#
-#-------------------------------------------------------------------------
+herbarium_origin = "Godfrey Herbarium FSU"
+herbarium_barcode = "barcode: " + str(12381237123)
+genus_species = "genus_species: "
+genus = "genus: "
+species = "species "
+Herbarium_Link = "url"
+AOFP_link = "other url"
+Contact = "contacnt@email.com"
+img_title = 'Agalinis tenuifolia_1.jpg'
+
+IPTC_Writer(program_description, digitizer, date_digitized, herbarium_origin, genus, herbarium_barcode, species, Herbarium_Link, AOFP_link)
+
+
+	Iptc.Application2.Subject String No Yes  13  236   Will hold the info
+    Iptc.Application2.ReleaseDate Date                8
+    Iptc.Application2.DateCreated Date No No 8 8
+    Iptc.Application2.DigitizationDate Date  No  No  8 8
+    Iptc.Application2.Byline String  No  Yes 0 32
+    Iptc.Application2.Headline  String  No  No  0 256 synopsis of content
+    Iptc.Application2.Contact   string  128
+    Iptc.Application2.Caption String No  No  0 2000
+
+'''
+
+
+def barcode_parse( s_string, code_pattern = "", regex= None):
+    # take a regex as input and ouput the groups
+    # can be used to clean and split file name
+    DEFAULT_REGEX = r""
+
+    if regex == None:
+        pattern = re.compile(DEFAULT_REGEX)
+        barcode, filename = pattern.match(s_string)
+    else:
+        pattern = re.compile(code_pattern)
+        barcode, filename = pattern.match(s_string)
+
+    return barcode, filename
+
 
 def size_img(imPath, ar = "AUTO"):
     """ Returns a resized image based on Aspect Ratio"""
 
     aspec_ratios = {"square":(650, 550, 1.18), "landscape":(650, 750, .86),
-    "portrait":(550, 450, 1.22), "slender":(400, 300, 1.3)}
+    "portrait":(550, 450, 1.22), "slender":(550, 400, 1.3)}
 
     def square(imgpath):
         im = Image.open(imgpath)
@@ -126,13 +197,13 @@ def size_img(imPath, ar = "AUTO"):
         return im.resize((width, new_height), resample = Image.LANCZOS)
 
     def aspect_detect(img_path):
-        bbox = Image.open(img_path).getbbox()
-        aspect_ratio = bbox[2] / bbox[3]
-        if (aspect_ratio <= .9):
+        width, height = Image.open(img_path).size
+        aspect_ratio = height / width
+        if (aspect_ratio <= 1):
             return landscape(imPath)
-        elif (.9 < aspect_ratio <= 1.195):
+        elif (1 < aspect_ratio <= 1.05):
             return square(imPath)
-        elif (1.195 < aspect_ratio <= 1.29):
+        elif (1.05 < aspect_ratio <= 1.29):
             return portrait(imPath)
         elif (aspect_ratio > 1.29):
             return slender(imPath)
@@ -147,7 +218,6 @@ def size_img(imPath, ar = "AUTO"):
         return slender(imPath)
     elif ar == "portrait":
         return portrait(imPath)
-
 
 def get_imgs(path):
     """ returns list of file paths to images that match the following formats
@@ -209,19 +279,21 @@ def draw_title(image_obj,
 
 
     if (xtent > xtent2): # draws the first set of rectangles
-        pencil.rectangle([(0,0), (xtent + (2*padding), ytent)],
+        pencil.rectangle([(0,0), (xtent + (2*padding), ytent + padding)],
                          fill=fill_color)
     else:
-        pencil.rectangle([(0,0), (xtent2 + (2*padding), ytent)],
+        pencil.rectangle([(0,0), (xtent2 + (2*padding), ytent + padding)],
                          fill=fill_color)
 
 
     if subtitl_str != "none":
         if (xtent > xtent2): # draws the subtitle set of rectangles (optnl)
-            pencil.rectangle([(0, ytent), (xtent + (2*padding), ytent + ytent2)],
+            pencil.rectangle([(0, ytent), (xtent + (2*padding), ytent +
+                            ytent2 + (padding/2) )],
                             fill= fill_color)
         else:
-            pencil.rectangle([(0, ytent), (xtent2 + (2*padding), ytent + ytent2)],
+            pencil.rectangle([(0, ytent), (xtent2 + (2*padding), ytent +
+                            ytent2 + (padding/2))],
                             fill= fill_color)
 
     pencil.text((padding, 0), title_string, font=title_font, fill=text_color)
@@ -231,10 +303,6 @@ def draw_title(image_obj,
 
     del pencil
     return image_obj
-
-
-
-
 
 
 if __name__ == '__main__':
