4c53ced4c5577ae5d3318839b4c96fbe72891e37
Struma <Struma_dev@protonmail.com>
2018-09-30 23:24:55 -0400

    added preview power

--- labelr/__init__.py
+++ labelr/__init__.py
@@ -120,7 +120,7 @@ Labelr.py --help for help")
         if options.img_preview:
             image_file.show()
         else:
-            image_file.save(os.path.join(args[1], os.path.basename(img)), "JPG")
+            image_file.save(os.path.join(args[1], os.path.basename(img)), "JPEG")
 
 
 def prompt_auth():
